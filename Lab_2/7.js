040b5872547e9f5f060278c9ce788323183f793c
AriBlank <arusa668@gmail.com>
2025-12-09 08:52:51 +0200

    lab2

--- Lab_2/7.js
+++ Lab_2/7.js
@@ -9,7 +9,6 @@ function fn() {
 
     console.log (object, object1);
 
-    //object = {name: 'Fluttershy'}; не можна присвоїти новий об'єкт для ідентифікатора який оголошений через константу
     object1 = {name: 'Fluttershy'};
 
     console.log(object1);
